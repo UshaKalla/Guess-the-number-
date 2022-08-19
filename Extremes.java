6d111f2517b54fb7f0ee741e2c5fbf156a2f6409
UshaKalla <111605331+UshaKalla@users.noreply.github.com>
2022-08-19 12:08:16 -0700

    Extremes.java

---
+++ Extremes.java
@@ -0,0 +1,35 @@
+public class Extremes
+{
+    Integer min;
+    Integer max;
+    
+    // Constructor
+    public Extremes()
+    {
+       min = Integer.MIN_VALUE;
+       max = Integer.MAX_VALUE; 
+       //Set min and max values 
+    }
+    
+    // Returns the difference
+    // max - number
+    public Integer maxDiff(Integer number)
+    {
+        return max - number; 
+    }
+
+    // Returns the difference
+    // min - number
+    public Integer minDiff(Integer number)
+    {
+        retunr min - number;
+    }
+    
+    // Returns a String representation
+    // in the form
+    // [min, max]
+    public String toString()
+    {
+        return "[" + min + ", " + max + "]";   
+    }
+}
